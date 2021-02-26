631f0cf2a24b6c8f0313876262eeeaf65fa9ba9f
Rafael de Oliveira Coelho <001.rocoelho@rte-intra.com.br>
2021-02-26 16:46:00 -0300

    Included app.urls in api_todo.urls and created all methods

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from app.views import TodoListAndCreate, TodoDetailChangeAndDelete
+
+from django.urls import path
+
+urlpatterns = [
+    path('', TodoListAndCreate.as_view()),
+    path('<int:pk>/', TodoDetailChangeAndDelete.as_view()),
+]
